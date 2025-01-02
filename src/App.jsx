2188c1dad9090730618793a2727117143351a24a
Muhammad Sefat <muhammadsefat55@gmail.com>
2025-01-02 09:36:37 +0600

    Start working banner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Banner from "./components/Banner";
 import Navbar from "./components/navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Banner />
     </>
   );
 }
