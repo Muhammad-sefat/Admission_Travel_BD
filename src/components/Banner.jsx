c5eecc0e6d16a645f8c0ce588933cabcce4040af
Muhammad Sefat <muhammadsefat55@gmail.com>
2025-01-04 08:40:59 +0600

    Complete Banner section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,17 +3,29 @@ import banner from "../assets/admission-banner.png";
 
 const Banner = () => {
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row">
-        <img src={banner} className="max-w-sm rounded-lg shadow-2xl" />
-        <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
+    <div className="hero bg-[#EDFBE2] min-h-screen">
+      <div className="hero-content flex-col lg:flex-row gap-10 p-8">
+        <div className="bg-[#EDFBE2] rounded-lg">
+          <img
+            src={banner}
+            alt="Banner"
+            className="rounded-lg max-w-xl"
+            style={{ display: "block", backgroundColor: "inherit" }}
+          />
+        </div>
+        <div className="text-left mb-10">
+          <h2 className="text-2xl font-bold">
+            Welcome to <br />
+            <span className="text-5xl font-bold"> Admission </span>
+            <br /> <span className="text-5xl font-bold">Traveler BD</span>
+          </h2>
           <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            We provide bus service for University admission candidates. Our
+            destination for Dhaka University, Jagannath University, Rajshahi
+            University, Chittagong University, Jahangirnagar University, CUET,
+            KUET, RUET, Krishi Guccho and many more universities.
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-primary">More About Us</button>
         </div>
       </div>
     </div>
